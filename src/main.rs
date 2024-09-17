c028b76dabb81390358a07a2826f573eaa769e49
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-17 18:29:59 +0800

    mutable chaneg example

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@
 
 
 fn main() {
+
     let x = 4; //immutable variables by default
-    println!("x is: {}", x);
+    println!("x is: {}", x); // by default cannot change "x" without redeclaring the variable
+
+    // to make "x" be able to be changes without being redefinied in rust you have to declare it as mutable in the variables declaration
+
+    let mut x = 5; //this is very complex and not simple in comparison to a language like python for example
+    println!("x is {}", x);
 }
