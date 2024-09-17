96a0dc4e384d09e66accc4db844271350aa2adb4
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-17 18:24:14 +0800

    comments header

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+// Shae Sullivan
+// 20643731
+// Programming Languages - Assignment
+// 17/09/24
+
+
+
 fn main() {
-    let x = 4;
+    let x = 4; //immutable variables by default
     println!("x is: {}", x);
 }
