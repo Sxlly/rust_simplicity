aa1f12dac5ad52089d64515261ea93469997e906
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-17 18:36:11 +0800

    colons

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ fn main() {
     // to make "x" be able to be changes without being redefinied in rust you have to declare it as mutable in the variables declaration
 
     let mut x = 5; //this is very complex and not simple in comparison to a language like python for example
-    println!("x is {}", x);
+    println!("x is: {}", x);
 
     //although the following code is valid in rust where as in python it would be "x is already definied"
     let y = 6;
-    println!("y is {}", y);
+    println!("y is: {}", y);
     let y = 8; //redefining is fine in rust as thats what it wants the user to do! as y as a variable is unmutable by default
-    println!("y is {}", y);
+    println!("y is: {}", y);
 
 }
