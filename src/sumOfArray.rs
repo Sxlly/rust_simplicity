b2143062623a6c7806608869ac7cc6f9951245a5
Sxlly <shaesullivan.buisness1@gmail.com>
2024-09-26 19:58:45 +0800

    fn main

--- src/sumOfArray.rs
+++ src/sumOfArray.rs
@@ -2,3 +2,11 @@
 // 20643731
 // Programming Languages - Assignment
 // 17/09/24
+fn main() {
+
+    let mut sum = 0u; 
+    for i in range(0u,numbers.len()) { 
+    sum += numbers[i]; 
+    }
+     
+}
