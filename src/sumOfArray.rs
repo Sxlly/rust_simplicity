667337ae535ed12e677e563d8a072e66cf70d373
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-04 16:53:27 +0800

    comments

--- src/sumOfArray.rs
+++ src/sumOfArray.rs
@@ -2,11 +2,12 @@
 // 20643731
 // Programming Languages - Assignment
 // 17/09/24
-fn main() {
 
-    let mut sum = 0u; 
-    for i in range(0u,numbers.len()) { 
-    sum += numbers[i]; 
+fn main() { //declare main funciton 
+
+    let mut sum = 0u; //declare variable of data type unsigned integer
+    for i in range(0u,numbers.len()) { //for index in range (unsigned integer 0 -> length of number array)
+    sum += numbers[i]; // 
     }
      
 }
