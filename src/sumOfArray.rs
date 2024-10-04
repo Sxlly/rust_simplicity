d9e2b57c7e0c47c576015d05975e04bf0be93f72
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-04 17:17:10 +0800

    changes

--- src/sumOfArray.rs
+++ src/sumOfArray.rs
@@ -21,11 +21,12 @@ fn main() { //declare main funciton
 
     let mut array: [i32; 5] = [10,20,30,40,50]; //declare array with all contents having a 32 bit signed integer data type and 5 index length
 
-    let arraysum: i32 = array.iter().sum(); // decalre arraysum variable as 32 bit signed integer 
+    let arraysum: i32 = array.iter().sum(); // decalre arraysum variable as 32 bit signed integer -> use array iter method to iterate through array and sum method to get sum
 
     println!(
-        "sum of array is {}"
+
+        "sum of array is {}",
         arraysum
-    )
+    );
 
 }
