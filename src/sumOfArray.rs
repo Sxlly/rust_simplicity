ce5997d528e45d6748a7790c0cbd573274ea28cc
Sxlly <shaesullivan.buisness1@gmail.com>
2024-10-04 17:19:33 +0800

    changes

--- src/sumOfArray.rs
+++ src/sumOfArray.rs
@@ -3,19 +3,6 @@
 // Programming Languages - Assignment
 // 17/09/24
 
-fn main() { //declare main funciton 
-
-    let mut numbers = [12,45,45,46,67];
-
-    let mut sum = 0u; //declare variable of data type unsigned integer
-    for i in range(0u,numbers.len()) { //for index in range (unsigned integer 0 -> length of number array)
-    sum += numbers[i]; // 
-    }
-
-    println!()
-     
-}
-
 
 fn main() { //declare main funciton 
 
